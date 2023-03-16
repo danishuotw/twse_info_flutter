85f3da1f804ffacca3b7682496a867e20c596bca
Danishuo <yusatrtru@gmail.com>
2023-03-16 18:07:22 +0800

    Added all screen we need

---
+++ lib/presentation/launch/launch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
